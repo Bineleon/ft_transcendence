d5bc920c66e859d704752124256d1f32717a16cc
TheChatou <chatougna@gmail.com>
2025-11-25 17:57:24 +0100

    fixxes2

--- backend/src/modules/tournaments/tournament.service.ts
+++ backend/src/modules/tournaments/tournament.service.ts
@@ -1,7 +1,6 @@
 import { getPrismaClient } from '../../shared/database/prisma.js';
 import type { TournamentMode, TournamentStatus } from '@prisma/client';
 import type { CreateTournamentDTO, TournamentResponse } from './tournament.model.js';
-import { request } from 'http';
 
 const prisma = getPrismaClient();
 
